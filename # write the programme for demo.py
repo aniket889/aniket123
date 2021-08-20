77de4c1b3af3cf76d9b506f77797f899edd6ff9e
aniket889 <aniketlepse60@gmail.com>
2021-08-20 16:19:03 +0530

    programmer for demo

---
+++ # write the programme for demo.py
@@ -0,0 +1,13 @@
+# write the programme for demo
+
+num=(float)(input("enter the number : "))
+if num>0:
+    print("positive number,")
+elif num==0:
+    print("zero number,")
+else :
+    print("negative number,")
+
+
+
+
